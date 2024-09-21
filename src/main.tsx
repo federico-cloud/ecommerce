513e1c6e154f8a9b881dc375d48a8ed08d1ff53f
Federico Galan <federico.egv@gmail.com>
2024-09-20 23:44:22 -0300

    feat: add React Query and update router configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { createBrowserRouter, RouterProvider, } from "react-router-dom"; 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Home } from './pages/home/Home.tsx';
 
 const router = createBrowserRouter([
   {
     path: "/home",
-    element: <div>Home</div>,
+    element: <Home/>,
   },
   {
     path: "/products",
@@ -14,9 +16,13 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
     <RouterProvider router={router}/>
     <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
